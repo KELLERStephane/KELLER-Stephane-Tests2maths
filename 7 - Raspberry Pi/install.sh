e512658bd8860c1265e2333d4d7fc766f54513f6
KELLER Stéphane <s.cas@gmx.fr>
2020-03-23 16:55:07 +0100

    Add files via upload

--- 7 - Raspberry Pi/install.sh
+++ 7 - Raspberry Pi/install.sh
@@ -12,7 +12,7 @@
 noir='\e[0;30m'
 gris='\e[1;30m'
 rougefonce='\e[0;31m'
-rougeclair='\e[1;31m'
+rougeclair='\e[;31m'
 rose='\e[1;31m'
 vertfonce='\e[0;32m'
 vertclair='\e[1;32m'
@@ -137,6 +137,22 @@ while "$boucle";do
         fi
 done
 
+boucle=true
+while "$boucle";do
+        echo -e -n "${bleuclair}\nInstallation de wiringpi pour l'utilisation des GPIO (o/n) : ${neutre}"
+        read repgpio
+        if [ "$repgpio" = "o" ] || [ "$repgpio" = "O" ]
+        then
+                echo -e "${jaune}wiringpi sera installé ${neutre}"
+                boucle=false
+        fi
+        if [ "$repgpio" = "n" ] || [ "$repgpio" = "N" ]
+        then
+                echo -e "${jaune}wiringpi ne sera pas installé  pour l'utilisatio des GPIO${neutre}"
+                boucle=false
+        fi
+done
+
 ### ===============================================================
 ### Installayion du gestionnaire de paquets "aptitude" si nécessaire
 ### ===============================================================
@@ -160,12 +176,6 @@ apt -y install wget
 ### ===============================================================
 echo -e "${vertclair}\nInstallation de git si nécessaire ${neutre}"
 apt -y install git
-
-# puis fixer le probleme de transport sur HTTPS
-#apt -y install ca-certificates apt-transport-https
-#wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
-# enfin on inscrit le dépot dans sources.list pour les prochaines mises à jour
-#echo "deb https://packages.sury.org/php/ buster main" | sudo tee /etc/apt/sources.list.d/php.list
 
 ### ===============================================================
 ### Installation du gestionnaire et éditeur de fichiers  "mc" si nécessaire
@@ -214,16 +224,18 @@ fi
 
 if [ "$repmotioneye" = "o" ] || [ "$repmotioneye" = "O" ]
 then
-	echo -e "${vertclair}\nInstallation de Motioneye avec le module de caméra CSI OV5647 pour le Rapsberry Pi ${neutre}"
+	echo -e "${vertclair}\nInstallation de Motioneye avec le module de caméra CSI OV5647 pour le Rapsberry Pi ${neutre}" 
+	echo -e "${rougeclair}\nNe pas oublier d'activer la caméra avec sudo raspi-config ${neutre}"
+
         if [ -d "/etc/motioneye" ]
 	then
-                echo -e "${cyanclair}Le répertoire d'installation /etc/motioneye existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}";
+                echo -e "${cyanclair}Le répertoire d'installation /etc/motioneye existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}"
                 rm -r /etc/motioneye
         fi
 
         if [ -d "/var/lib/motioneye" ]
 	then
-                echo -e "${cyanclair}Le répertoire média /var/lib/motioneye existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}";
+                echo -e "${cyanclair}Le répertoire média /var/lib/motioneye existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}"
                 rm -r /var/lib/motioneye
         fi
 
@@ -238,10 +250,14 @@ then
         echo -e "${vertclair}\nDésactivation de la led de la caméra CSI OV5647 pour le Rapsberry Pi ${neutre}"
 	if grep -q "disable_camera_led=1" "/boot/config.txt"
 	then
-                echo -e "${cyanclair}La désactivation de la led de la caméra est déja active dans /boot/config.txt ${neutre}";
+                echo -e "${cyanclair}La désactivation de la led de la caméra est déja active dans /boot/config.txt ${neutre}"
         else
         	echo "disable_camera_led=1" | sudo tee -a /boot/config.txt
         fi
+
+	echo -e "${vertclair}\nMise à jour des paquets dépendants si nécessaire ${neutre}"
+        apt -y install python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
+        apt -y install ffmpeg libmariadb3 libpq5 libmicrohttpd12
 
         echo -e "${vertclair}\ntéléchargement de Motioneye ${neutre}"
 	if [ -e /home/pi/pi_buster* ]
@@ -250,13 +266,8 @@ then
 		echo -e "${cyanclair}Effacement du fichier puis téléchargement du nouveau fichier ${neutre}"
 		rm /home/pi/pi_buster*
 	fi
-
-        echo -e "${vertclair}\nMise à jour des paquets dépendants si nécessaire ${neutre}"
-        apt -y install python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
-        apt -y install ffmpeg libmariadb3 libpq5 libmicrohttpd12
-
-        echo -e "${vertclair}\nInstallation de Motioneye ${neutre}"
         wget https://github.com/Motion-Project/motion/releases/download/release-4.2.2/pi_buster_motion_4.2.2-1_armhf.deb
+        echo -e "${vertclair}\nInstallation de Motioneye ${neutre}"
         dpkg -i pi_buster_motion_4.2.2-1_armhf.deb
         pip install motioneye
 
@@ -280,7 +291,7 @@ then
 
 #        if [ -d "/etc/apache2" ]
 #        then
-#                echo -e "${cyanclair}Le répertoire d'installation d'Apache2 /etc/apache2 existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}";
+#                echo -e "${cyanclair}Le répertoire d'installation d'Apache2 /etc/apache2 existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}"
 #                rm -r /etc/apache2
 #        fi
         aptitude install apache2 -y
@@ -291,18 +302,29 @@ then
 		rm /var/www/html/index.html
 	fi
 
-        echo -e "${vertclair}\nSécuristion d'Apache2. ${neutre}"
-        if [ -d "/var/www/passwd" ]
-        then
-                echo -e "${cyanclair}Le répertoire de mots de passe /var/www/passwd existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}";
-                rm -r /var/www/passwd
-        fi
-        echo -e "${vertclair}\nCréation du répertoire de mot de passe sécurisé /var/wwww/passwd ${neutre}"
-        cd /var/www/
-        mkdir passwd
-        echo -e -n "${vertclair}\nSaisir le nom de l'utilisateur principal pour Apache2 : ${neutre}"
-        read username
-        htpasswd -c /var/www/passwd/passwords "$username"
+	boucle=true
+	while "$boucle";do
+        	echo -e "${vertclair}\nSécuristion d'Apache2. ${neutre}"
+	        if [ -d "/var/www/passwd" ]
+	        then
+        	        echo -e "${cyanclair}Le répertoire de mots de passe /var/www/passwd existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}"
+                	rm -r /var/www/passwd
+	        fi
+	        echo -e "${vertclair}\nCréation du répertoire de mot de passe sécurisé /var/wwww/passwd ${neutre}"
+	        cd /var/www/
+	        mkdir passwd
+	        echo -e -n "${vertclair}\nSaisir le nom de l'utilisateur principal pour Apache2 : ${neutre}"
+	        read username
+	        htpasswd -c /var/www/passwd/passwords "$username"
+		erreur=$?
+#		echo -e "L'erreur est $erreur"
+		if [ "$erreur" = 0 ]
+		then
+			boucle=false
+		else
+			echo -e "${rougeclair}Erreur. Recommencer ${neutre}"
+		fi
+	done
 
         echo -e "${vertclair}\nModification du fichier /etc/apache2/apache2.conf pour sécuriser l'accès à Apache2 ${neutre}"
         if grep -q "AuthName \"ACCES PROTEGE\"" "/etc/modules"
@@ -351,7 +373,7 @@ then
 
         if [ -d "/etc/fail2ban" ]
         then
-                echo -e "${cyanclair}Le répertoire d'installation de Fail2ban /etc/fail2ban existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}";
+                echo -e "${cyanclair}Le répertoire d'installation de Fail2ban /etc/fail2ban existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}"
 	        echo -e "${vertclair}/etc/fail2ban/jail.copy -> /etc/fail2ban/jail.conf ${neutre}"
         	echo -e "${vertclair}/etc/fail2ban/fail2ban.copy -> /etc/fail2ban/fail2ban.conf ${neutre}"
      	   	cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.copy
@@ -376,6 +398,7 @@ then
 	fi
 	wget -P /etc/fail2ban/jail.d/ https://raw.githubusercontent.com/KELLERStephane/KELLER-Stephane-Tests2maths/master/7%20-%20Raspberry%20Pi/custom.conf
 	boucle=true
+
 	while "$boucle";do
 	        echo -e "${bleuclair}\nSaisir l'adresse mail pour les messages de Fail2ban : ${neutre}"
 	        read repmaila
@@ -389,6 +412,7 @@ then
 	                echo -e "${jaune}Adresse mail différente. Recommencer"
         	fi
 	done
+
         echo -e "${vertclair}Sauvegarde du fichier de configuration personnalisable de Fail2ban ${neutre}"
         echo -e "${vertclair}/etc/fail2ban/jail.d/custom.conf -> /etc/fail2ban/jail.d/custom.copy ${neutre}"
 	cp /etc/fail2ban/jail.d/custom.conf /etc/fail2ban/jail.d/custom.copy
@@ -413,7 +437,7 @@ then
 
         if [ -d "/home/pi/script" ]
         then
-                echo -e "${cyanclair}Le répertoire /home/pi/script existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}";
+                echo -e "${cyanclair}Le répertoire /home/pi/script existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}"
                 rm -r /home/pi/script
         fi
 
@@ -463,7 +487,7 @@ then
         echo -e "${vertclair}\nTéléchargement de Fail2map ${neutre}"
 	if [ -d "/var/www/html/fail2map" ]
 	then
-		echo -e "${cyanclair}Le répertoire /var/www/html/fail2map existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}";
+		echo -e "${cyanclair}Le répertoire /var/www/html/fail2map existe déja. Suppression du répertoire avant la nouvelle installation  ${neutre}"
 		rm -r /var/www/html/fail2map
  	fi
         git clone https://github.com/mvonthron/fail2map /var/www/html/fail2map
@@ -515,6 +539,21 @@ then
         sed '/'"$L1"'/ c\'"$L2"''"$L3"'' /var/www/html/fail2map/js/maps.js >/home/pi/maps.js
         mv /home/pi/maps.js /var/www/html/fail2map/js/maps.js
 fi
+
+### ===============================================================
+### GPIO
+### ===============================================================
+
+if [ "$repgpio" = "o" ] || [ "$repgpio" = "O" ]
+then
+        echo -e "${bleuclair}\nInstallation de wiringpi pour l'utilisation des GPIO (nécessite Fail2ban) ${neutre}"
+        echo -e "${rougeclair}\nNe pas oublier d'activer les GPIO avec sudo raspi-config ${neutre}"
+	cd /tmp
+	wget https://project-downloads.drogon.net/wiringpi-latest.deb
+	sudo dpkg -i wiringpi-latest.deb
+        echo -e "${bleuclair}\nExecuter la commande gpio readall pour voir la configuration des broches ${neutre}"
+fi
+
 
 ### ===============================================================
 ### Copyright
