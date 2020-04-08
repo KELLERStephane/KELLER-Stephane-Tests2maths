c76ef3342d1f04f6790f4cdea5515c4fdd29747e
KELLER Stéphane <s.cas@gmx.fr>
2020-04-08 11:54:23 +0200

    Delete jails.sh

--- 7 - Raspberry Pi/jails.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#lancer le script en sudo 
-JAILS=$(fail2ban-client status | grep " Jail list:" | sed 's/`- Jail list:\t//g' | sed 's/,//g')
-for j in $JAILS
-do
-echo "$j $(fail2ban-client status $j | grep " Currently banned:" | sed 's/   |- Currently banned:\t//g')"
-done
-
