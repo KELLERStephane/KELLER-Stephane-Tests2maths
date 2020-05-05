8d09225129c1df9d655662c91e893d3a90585315
KELLER Stéphane <s.cas@gmx.fr>
2020-05-05 17:47:37 +0200

    Add files via upload

---
+++ 7 - Raspberry Pi/Ressources/moons/README.md
@@ -0,0 +1,14 @@
+----------------------------------------------------
+|    SK Lib - Calcul des dates de phase lunaire    |
+----------------------------------------------------
+
+Vous pouvez l'installer avec pip:
+
+    pip install moons
+
+Exemple d'usage:
+
+    >>> import moonsk
+    >>> lunar_phase(day, month, year)
+
+Ce code est sous licence WTFPL.
---
+++ 7 - Raspberry Pi/Ressources/moons/moons/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""From a date given in parameter,
+the algorithm returns dates for :
+- the new moon;
+- the first quarter;
+- the full moon;
+- last quarter
+
+Author : Keller Stéphane.
+Enseignant de mathématiques, physique chimie, informatique et SNT.
+Lycée agricole Louis Pasteur - Marmilhat.  B.P. 116 - 63 370 Lempdes
+stephane.keller@yahoo.com
+https://github.com/KELLERStephane/KELLER-Stephane-Tests2maths
+"""
+
+__version__ = "1.0"
+
+#from moonssk import angle
+#from moonssk import jj2date
+#from moonssk import calcul_Ci
+from moonssk import lunar_phase
+#from moonssk import between_dates
+
