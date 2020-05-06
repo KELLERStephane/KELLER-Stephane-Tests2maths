bd1e3ed72be45daec6f03d5d0d8b3ef036f97872
KELLER Stéphane <s.cas@gmx.fr>
2020-05-06 09:14:10 +0200

    Add files via upload

---
+++ 7 - Raspberry Pi/Ressources/lune/README.md
@@ -0,0 +1,14 @@
+---------------------------------------------------
+|    lune - Calcul des dates de phase lunaire    |
+---------------------------------------------------
+
+Vous pouvez l'installer avec pip:
+
+    pip install lune
+
+Exemple d'usage:
+
+    >>> from lune import phase
+    >>> phase.lunar_phase(day, month, year)
+
+Ce code est sous licence WTFPL.
---
+++ 7 - Raspberry Pi/Ressources/lune/lune/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""From a date given in parameter,
+the algorithm returns dates for :
+- the new moon;
+- the first quarter;
+- the full moon;
+- last quarter
+Author : Keller Stéphane.
+Enseignant de mathématiques, physique chimie, informatique et SNT.
+Lycée agricole Louis Pasteur - Marmilhat.  B.P. 116 - 63 370 Lempdes
+stephane.keller@yahoo.com
+https://github.com/KELLERStephane/KELLER-Stephane-Tests2maths
+"""
+
+__version__ = "1.0"
+
+from lune.phase import angle
+from lune.phase import jj2date
+from lune.phase import calcul_Ci
+from lune.phase import lunar_phase
+from lune.phase import between_dates
