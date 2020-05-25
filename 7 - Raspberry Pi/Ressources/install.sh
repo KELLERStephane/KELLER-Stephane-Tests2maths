b69ca112ab7501b891feba57d73dd2f090c5cc12
KELLER Stéphane <s.cas@gmx.fr>
2020-05-25 10:18:19 +0200

    Add files via upload

--- 7 - Raspberry Pi/Ressources/install.sh
+++ 7 - Raspberry Pi/Ressources/install.sh
@@ -119,96 +119,6 @@ while $boucle_principale;do
             L1='server 0.fr.pool.ntp.org'
             echo -e $L1>/etc/ntp.com
 
-            echo -e "${bleuclair}\nInstallation de Python2 et de Python3 et de ses modules si nécessaire ${neutre}"
-            apt·-y·install·python2
-            apt install -y python3
-            echo -e "${vertclair}\nModification da la version par défaut de python en python3 si nécessaire ${neutre}"
-            update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-            update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
-            echo -e "${vertclair}\nChoix de la version de Python par défaut : ${neutre}"
-            #echo 1 pour Python 2 par défaut et echo 2 pour Python 3 par défaut
-            echo 1 | sudo update-alternatives --config python
-            echo -e -n "${vertclair}\nLa version de Python par défaut est : ${neutre}"
-            python --version
-            apt-get install libatlas-base-dev
-            
-            ver=$(python -c"import sys; print(sys.version_info.major)")
-            if [ $ver -eq 3 ]; then
-                #installation des paquets pour Python3
-                echo -e "${vertclair}\nInstallation de pip, dev, pil, setuptools, wheel, requests pour python3 si nécessaire ${neutre}"
-                apt install -y python3-pip
-                apt install -y python3-dev
-                apt install -y python3-setuptools
-                python3 -m pip install --upgrade pip
-                python3 -m pip install --upgrade Pillow
-                python3 -m pip install --upgrade matplotlib
-                python3 -m pip install --upgrade pip setuptools wheel
-                python3 -m pip install requests
-            elif [ $ver -eq 2 ]; then
-                #installation des paquets pour Python2
-                echo -e "${vertclair}\nInstallation de pip, dev, pil, setuptools, wheel, requests pour python2 si nécessaire ${neutre}"
-                apt install -y python-pip
-                apt install -y python-dev
-                apt install -y python-setuptools
-                python -m pip install --upgrade pip
-                python -m pip install --upgrade Pillow
-                python -m pip install --upgrade matplotlib
-                python -m pip install --upgrade pip setuptools wheel
-                python -m pip install requests
-
-                ####################################################################################
-                # Installation des environnements virtuels Python2 et Python2
-                ####################################################################################
-
-                pip install --user virtualenv virtualenvwrapper
-
-                grep -i "/usr/bin/python" "/home/pi/.profile" >/dev/null
-                if [ $? = 0 ];then
-                    echo -e "${vertclair}\nLe fichier /home/pi/.profile a déja été modifié ${neutre}"
-                else
-                    if [ -f /home/pi/script/python_startup_script.py ] ; then
-                        echo -e "${cyanclair}\nLe fichier /home/pi/script/python_startup_script.py existe déjà ${neutre}"
-                        echo -e "${cyanclair}Effacement du fichier puis téléchargement du nouveau fichier ${neutre}"
-                        rm /home/pi/script/python_startup_script.py*
-                    fi
-                    wget -P /home/pi/script $lien_github_raw/python_startup_script.py
-                    chown pi:pi /home/pi/script $lien_github_raw/python_startup_script.py
-
-                    echo -e "${vertclair}\nModification du fichier /home/pi/.profile ${neutre}"
-                    PYV2=`python -c "import sys;t='python{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
-                    path_python2=`echo "which $PYV2"`
-                    echo -e "${bleuclair}\nLe chemin de Python2 est `$path_python2` ${neutre}"
-                    PYV3=`python3 -c "import sys;t='python{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
-                    path_python3=`echo "which $PYV3"`
-                    echo -e "${bleuclair}\nLe chemin de Python3 est `$path_python3` ${neutre}"
-
-                    L1='\nexport WORKON_HOME=/home/pi/.virtualenvs'
-                    L2='\nmkdir -p $WORKON_HOME'
-                    L3="\nexport VIRTUALENVWRAPPER_PYTHON=/usr/bin/$PYV2"
-                    L4='\nsource /usr/local/bin/virtualenvwrapper.sh'
-                    L5='\nexport VIRTUALENVWRAPPER_ENV_BIN_DIR=bin'
-                    L6='\nexport PYTHONSTARTUP=/home/pi/script/python_startup_script.py'
-                    echo -e $L1$L2$L3$L4$L5$L6>> /home/pi/.profile # ajout des lignes dans le fichier
-                    echo -e "${bleuclair}\nFichier /home/pi/.profile modifié ${neutre}"
-                    source /home/pi/.profile
-
-                    echo -e "${bleuclair}\nCréation environnement Python2 ${neutre}"
-                    mkvirtualenv python2env -p `$path_python2`
-                    echo -e "${bleuclair}\nCréation environnement Python3 ${neutre}"
-                    mkvirtualenv python3env -p `$path_python3`
-
-                    echo -e "${rougeclair}\nPour activer un environnement Python, ${neutre}"
-                    echo -e "${rougeclair}dans n'importe quelle fenêtre de terminal, entrez : ${neutre}"
-                    echo -e "${rougeclair}workon python2env pour l'environnement pour Python2 ${neutre}"
-                    echo -e "${rougeclair}workon python3env pour l'environnement pour Python3 ${neutre}"
-                    echo -e "${rougeclair}\nPour désactiver l'environnment Python3, ${neutre}"
-                    echo -e "${rougeclair}dans n'importe quelle fenêtre de terminal, entrez : ${neutre}"
-                    echo -e "${rougeclair}deactivate ${neutre}"
-                fi
-            else
-                echo -e "${vertclair}\nVersion de Python inconnue ${neutre}"
-            fi
-
             if [[ $CHOIX =~ "Debug" ]]; then
                 echo -e "${violetclair}\nFin des mises à jour. Appuyer sur Entrée pour poursuivre l'Installation ${neutre}"
             read
@@ -530,6 +440,20 @@ while $boucle_principale;do
             echo -e "${bleuclair}\nInstallation de Motioneye avec le module de caméra CSI OV5647 pour le Rapsberry Pi ${neutre}"
             echo -e "${rougeclair}Ne pas oublier d'activer la caméra avec sudo raspi-config ${neutre}"
 
+            echo -e "${bleuclair}\nInstallation de Python2 et mise à jour si nécessaire ${neutre}"
+            apt install -y·python2
+            apt --upgrade python2
+            #installation des paquets pour Python2
+            echo -e "${vertclair}\nInstallation de pip, dev, pil, setuptools, wheel, requests pour python2 si nécessaire ${neutre}"
+            apt install -y python-pip
+            apt install -y python-dev
+            apt install -y python-setuptools
+            python -m pip install --upgrade pip
+            python -m pip install --upgrade Pillow
+            python -m pip install --upgrade matplotlib
+            python -m pip install --upgrade pip setuptools wheel
+            python -m pip install requests
+
             if [ -d "/etc/motioneye" ]; then
                 echo -e "${cyanclair}Le répertoire d'installation /etc/motioneye existe déjà. Suppression du répertoire avant la nouvelle installation  ${neutre}"
                 rm -r /etc/motioneye
@@ -808,6 +732,32 @@ while $boucle_principale;do
                 "DEL" "Désinstallation des capteurs et programmes associés" OFF 3>&1 1>&2 2>&3)
 
                 exitstatus=$?
+
+                #installation des paquets pour Python3
+                echo -e "${vertclair}\nInstallation de pip, dev, pil, setuptools, wheel, requests pour python3 si nécessaire ${neutre}"
+                apt install -y python3-pip
+                apt install -y python3-dev
+                apt install -y python3-setuptools
+                python3 -m pip install --upgrade pip
+                python3 -m pip install --upgrade Pillow
+                python3 -m pip install --upgrade matplotlib
+                python3 -m pip install --upgrade pip setuptools wheel
+                python3 -m pip install requests
+ 
+                echo -e "${bleuclair}\nInstallation de Python3 et mise à jour si nécessaire ${neutre}"
+                apt install -y python3
+                apt --upgrade python3
+#                echo -e "${vertclair}\nModification da la version par défaut de python en python3 si nécessaire ${neutre}"
+#                update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+#                update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+                echo -e "${vertclair}\nChoix de la version de Python par défaut : ${neutre}"
+                #echo 1 pour Python 2 par défaut et echo 2 pour Python 3 par défaut
+                echo 1 | sudo update-alternatives --config python
+                echo -e -n "${vertclair}\nLa version de Python par défaut est : ${neutre}"
+                python --version
+                apt-get install libatlas-base-dev
+            
+                ver=$(python -c"import sys; print(sys.version_info.major)")
 
                 if [[ $exitstatus = 0 ]]; then
                     echo -e -n "${jauneclair}\t =======================================  \n ${neutre}"
