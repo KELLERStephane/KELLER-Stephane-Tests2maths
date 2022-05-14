00c9f3019b7223ad5a1ab1702e764e7996ef87e3
KELLER Stéphane <s.cas@gmx.fr>
2022-05-14 17:29:11 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# coding: utf-8
+
+###Récuparation du nom de l'utilisateur courant
+utilisateur=`cat /etc/passwd | grep "home/" | grep -v "nologin" | cut -d ":" -f1`
+echo -e "${jauneclair}L'utilisateur est $utilisateur \n ${neutre}"
+cd /home/$utilisateur
+
+#téléchargement du fichier d'installation du raspberry
+wget https://raw.githubusercontent.com/KELLERStephane/KELLER-Stephane-Tests2maths/master/7%20-%20Raspberry%20Pi/Ressources/install.sh
+chmod +x install.sh
+
+#attente de l'appui sur la touche entrée
+echo -e -n "${violetclair}\nFin du téléchargement. Appuyer sur Entrée pour poursuivre l'Installation \n ${neutre}"
+read
+
+./install.sh
